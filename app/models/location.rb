dc002473b2308406e0d009c97bf361ce9710ff21
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-05 07:12:15 -0800

    add hometown fk to users and set up relationships

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,4 @@
 class Location < ApplicationRecord
+  has_many :residents, foreign_key: :hometown_id
+  has_many :courses
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,3 @@
 class User < ApplicationRecord
+  belongs_to :hometown, class_name: "Location"
 end
