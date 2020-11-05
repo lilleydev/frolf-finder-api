4561aed98aba06bba78702fe1aafecba9fc852ec
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-05 15:35:03 -0800

    add serializer for user model and show action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  def show
+    user_json = UserSerializer.new(@user).serialized_json
+
+    render json: user_json
+  end 
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end 
 end
