8e89727454f0a9e0537af2acdf4ad577e4ce7015
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-05 07:01:21 -0800

    create locations resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # namespace to site thediscgolfproject.com/api/v1/resource
-
+  
   namespace :api do
     namespace :v1 do 
       resources :courses
+      resources :locations
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
