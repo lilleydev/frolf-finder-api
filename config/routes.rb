09ead4558252d0e4387c455f4a092bce069b569b
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-07 10:09:44 -0800

    add get route for user session to get current user

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # namespace to site thediscgolfproject.com/api/v1/resource
   post "/api/v1/login", to: "api/v1/sessions#create"
+  get "/api/v1/get_current_user", to: "api/v1/sessions#get_current_user"
   namespace :api do
     namespace :v1 do 
       resources :courses
