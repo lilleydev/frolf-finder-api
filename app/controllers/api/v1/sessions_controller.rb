86599d33d2a75b6e2cc9cbef5f9af344b44908ae
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-06 18:15:27 -0800

    build create action

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,4 +1,13 @@
 class Api::V1::SessionsController < ApplicationController
   def create
+    # binding.pry
+      @user = User.find_by(username: params[:session][:username])
+    if @user 
+      # add bcrypt to be able to authenticate
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: { error: "Invalid Credentials"}
+    end 
   end
 end
