cd6971f8ed154875e3725b905e87ba5aa2a55baf
blu3humm1ngb1rd <bethjeanlilley@gmail.com>
2020-11-07 20:43:04 -0800

    add serialization for current_user

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::SessionsController < ApplicationController
     if @user 
       # add bcrypt to be able to authenticate
       session[:user_id] = @user.id
-      render json: @user
+      render json: UserSerializer.new(@user)
     else
       render json: { error: "Invalid Credentials"}
     end 
@@ -13,12 +13,14 @@ class Api::V1::SessionsController < ApplicationController
 
   def get_current_user
     if logged_in?
-      render json: current_user
+      render json: UserSerializer.new(current_user)
     else
       render json: { error: "No one logged in"}
     end 
   end 
+# user_json = UserSerializer.new(@user).serialized_json
 
+#     render json: user_json
   def destroy 
     session.clear 
     render json: { notice: "sucessfully logged out"}, status: :ok
